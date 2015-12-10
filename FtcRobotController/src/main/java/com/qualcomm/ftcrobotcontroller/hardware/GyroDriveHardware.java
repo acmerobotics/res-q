fffca1b671e73c07efa40dfe13d1743c93c6839e
Ryan Brott <rcbrott@gmail.com>
2015-12-09 22:42:56 -0800

    Basic ultrasonic drive support. Motor stepping implementation with PID.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/hardware/GyroDriveHardware.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/hardware/GyroDriveHardware.java
@@ -1,13 +1,15 @@
 package com.qualcomm.ftcrobotcontroller.hardware;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.util.RobotLog;
 
 /**
  * Created by Admin on 12/3/.1515.
  */
-public class GyroDriveHardware extends DriveHardware {
+public class GyroDriveHardware extends HardwareInterface {
 
+    public DriveHardware driveHardware;
     public I2cGyroHardware gyroHardware;
 
     private TurnState turnState = TurnState.NOT_TURNING;
@@ -28,29 +30,24 @@ public class GyroDriveHardware extends DriveHardware {
         public void onTurnFinished();
     }
 
-    public GyroDriveHardware(I2cGyroHardware gyroHardware) {
+    public GyroDriveHardware(DriveHardware driveHardware, I2cGyroHardware gyroHardware) {
+        this.driveHardware = driveHardware;
         this.gyroHardware = gyroHardware;
     }
 
     @Override
     public void init(OpMode mode) {
-        super.init(mode);
         opMode = mode;
     }
 
     @Override
     public void loop(double timeSinceLastLoop) {
-        super.loop(timeSinceLastLoop);
-        opMode.telemetry.addData("Set Point", setPoint);
-        opMode.telemetry.addData("Target", targetHeading);
-        opMode.telemetry.addData("Raw", gyroHardware.getHeading());
-        opMode.telemetry.addData("Normalized", getNormalizedHeading());
         gyroHardware.loop(timeSinceLastLoop);
         if (
             (turnState.equals(TurnState.LEFT) && getNormalizedHeading() <= targetHeading) ||
             (turnState.equals(TurnState.RIGHT) && getNormalizedHeading() >= targetHeading)
             ) {
-            stopMotors();
+            driveHardware.stopMotors();
             turnState = TurnState.NOT_TURNING;
             if (callback != null) callback.onTurnFinished();
             callback = null;
@@ -61,7 +58,7 @@ public class GyroDriveHardware extends DriveHardware {
         callback = cb;
         resetHeading();
         targetHeading = -degrees;
-        setMotorSpeeds(-.15, .15);
+        driveHardware.setMotorSpeeds(-.15, .15);
         turnState = TurnState.LEFT;
     }
 
@@ -69,7 +66,7 @@ public class GyroDriveHardware extends DriveHardware {
         callback = cb;
         resetHeading();
         targetHeading = degrees;
-        setMotorSpeeds(.15, -.15);
+        driveHardware.setMotorSpeeds(.15, -.15);
         turnState = TurnState.RIGHT;
     }
 
@@ -78,7 +75,6 @@ public class GyroDriveHardware extends DriveHardware {
         turnLeft(degrees, null);
         while (isTurning()) {
             try {
-                opMode.telemetry.addData("Going", "Yay!");
                 ((LinearRobotController) opMode).waitOneFullHardwareCycle();
             } catch (InterruptedException e) {
                 RobotLog.e(e.getMessage());
