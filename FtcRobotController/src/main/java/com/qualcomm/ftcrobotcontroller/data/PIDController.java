cf21278b2101ae2b711db5908407f5647d43ebc9
Ryan Brott <r.brott@comcast.net>
2016-02-04 17:02:07 -0800

    Added timestamped data and built in PID controller support

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/data/PIDController.java
@@ -0,0 +1,29 @@
+package com.qualcomm.ftcrobotcontroller.data;
+
+/**
+ * Created by Admin on 2/4/2016.
+ */
+public class PIDController<T extends Double> {
+
+    private TimestampedData<T> lastData = null;
+    private double sumError = 0, output = 0;
+    private double kP, kI, kD;
+
+    public PIDController(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+
+    public void add(TimestampedData<T> data) {
+        double error = data.getData().doubleValue() - lastData.getData().doubleValue(),
+               dt = ((double) data.getTimeSince(lastData)) / Math.pow(10, 6),
+               rate = error / dt;
+        sumError += error * dt;
+        output = kP * error + kI * sumError + kD * rate;
+    }
+
+    public double getOutput() {
+        return output;
+    }
+}
